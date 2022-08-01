f5f6f87f64ae7f2b4b18df7492e14f15bbd9e007
maruf12 <maruf.harsono@gmail.com>
2022-08-01 15:45:28 +0700

    testing env netlify

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Welcome to financial tracker</h1>
+    <p>{{ baseUrlType }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      baseUrlType: process.env.baseUrl
+    }
+  }
 }
 </script>
